9c191e826450b76565161d80dfbe712a30cbc522
Josh Geden <joshgeden10@gmail.com>
2021-05-05 16:34:51 -0400

    worked on refresh button

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,24 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { Widget } from '@lumino/widgets';
+import { IStatusBar } from '@jupyterlab/statusbar';
+
+const API_URL = 'https://jupyter-dev.nersc.gov/services/announcement/latest';
+
+class ButtonWidget extends Widget {
+  public constructor(options = { node: document.createElement('button') }) {
+    super(options);
+    this.node.onclick = refreshAnnouncements;
+  }
+}
+
+async function refreshAnnouncements() {
+  const response = await fetch(API_URL);
+  const data = response.json();
+
+  console.log(data);
+}
 
 /**
  * Initialization data for the nersc-refresh-announcements extension.
@@ -9,8 +27,19 @@ import {
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'nersc-refresh-announcements:plugin',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension nersc-refresh-announcements is activated!');
+  requires: [IStatusBar],
+  activate: (app: JupyterFrontEnd, statusBar: IStatusBar) => {
+    console.log(
+      'JupyterLab extension nersc-refresh-announcements is activated!'
+    );
+
+    const statusWidget = new ButtonWidget();
+    statusWidget.node.textContent = 'New Announcement ⚠️';
+
+    statusBar.registerStatusItem('test', {
+      align: 'middle',
+      item: statusWidget
+    });
   }
 };
 
