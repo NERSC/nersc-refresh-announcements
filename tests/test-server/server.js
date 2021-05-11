06dcf7f1014db7e1a38ece27f0f9e9eadab5f58b
Josh Geden <joshgeden10@gmail.com>
2021-05-11 09:36:03 -0400

    moved test server and created workflow

---
+++ tests/test-server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000 || process.env.PORT;
+
+const emptyAnnouncement = process.argv.includes('--empty-announcement');
+
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`App listening at port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.status(200);
+    if (emptyAnnouncement) {
+        res.json({});
+    } else {
+        res.json({
+            user: 'rcthomas',
+            announcement:
+                'This is a test of the Jupyter announcement system. This is only a test. Here is a link to the <a href="https://www.nersc.gov/live-status/motd/">NERSC MOTD</a>.',
+            timestamp: '2021-05-05T13:05:57.957231'
+        });
+    }
+});
